10bdda7b6b541cec307f1b031bf20c3f6060df1d
Hariom Singh <hariomsingh0398@gmail.com>
2023-09-21 07:50:13 +0530

    array reverse

---
+++ Array/ReverseArray.cpp
@@ -0,0 +1,40 @@
+#include<iostream>
+using namespace std;
+
+// This program will find the Reverse of an Array
+
+void printArray(int arr[], int n) {
+    for (int i = 0; i < n; i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+
+void reverse(int arr[], int n) {
+    int start = 0;
+    int end = n - 1;
+    while (start <= end) {
+        swap(arr[start], arr[end]);
+        start++;
+        end--;
+    }
+}
+
+int main() {
+    int arr1[] = {1, 2, 3, 4, 5, 8}; // Even Array
+    int arr2[] = {6, 4, 9, 2, 5};    // Odd Array
+
+    cout << "Array 1 before reverse : ";
+    printArray(arr1, 6);
+    reverse(arr1, 6);
+    cout << "Array 1 after reverse : ";
+    printArray(arr1, 6); // 1st Array reversed
+
+    cout << "Array 2 before reverse : ";
+    printArray(arr2, 5);
+    reverse(arr2, 5);
+    cout << "Array 2 after reverse : ";
+    printArray(arr2, 5); // 2nd Array reversed
+
+    return 0;
+}
