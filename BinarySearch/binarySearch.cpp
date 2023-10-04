b3f59568a8dcc1ae6529353c6f7e36706a1603de
Hariom Singh <hariomsingh0398@gmail.com>
2023-10-04 08:09:30 +0530

    Binary Search in C++

---
+++ BinarySearch/binarySearch.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+
+// Performing Binary search Algo.
+
+int binarySearch(int arr[], int size, int key) {
+    int start = 0;
+    int end = size - 1;
+
+    while (start <= end) {
+        int mid = start + (end - start) / 2;
+        if (arr[mid] == key) {
+            return mid; // returning key element index
+        }
+        if (key > arr[mid]) { // Searching right side elements from mid
+            start = mid + 1;
+        }
+        else { // Searching left side elements from mid
+            end = mid - 1;
+        }
+    }
+
+    return -1;
+}
+
+int main() {
+    int even[6] = {1, 3, 4, 6, 8, 9};
+    int odd[5] = {3, 4, 6, 9, 8};
+    int key;
+    cout << "enter key element : ";
+    cin >> key;
+    cout << "Return -1 if key is not present in the array" << endl << endl;
+    int indexEven = binarySearch(even, 6, key);
+    cout << "the key " << key << " in even array index is " << indexEven << endl;
+    int indexOdd = binarySearch(odd, 5, key);
+    cout << "the key " << key << " in odd array index is " << indexOdd << endl;
+
+    return 0;
+}
