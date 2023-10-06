3cacca53729512717277d8df20562753cea0595b
Hariom Singh <hariomsingh0398@gmail.com>
2023-10-06 08:21:39 +0530

    Pivot element in Array

---
+++ BinarySearch/findPivot.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+using namespace std;
+
+//you have given a rotated array find the Pivot element of the Array
+
+int findPivot(int arr[],int n){
+    int s = 0;
+    int e = n-1;
+    int mid = s+(e-s)/2;
+
+    while(s<e){
+        if(arr[mid]>=arr[0]){
+            s = mid+1;
+        }
+        else{
+            e = mid;
+        }
+        mid = s+(e-s)/2;
+    }
+    return e;//You can also retur s
+}
+
+int main(){
+    int arr[]={4,5,6,8,1,2,3};
+
+    cout<<"Pivot element index is : "<<findPivot(arr,7);
+
+    return 0;
+   
+}
